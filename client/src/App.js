a7583cfddad077d52caf31e100c1be5069d8df37
Daisuke Tsuji <dim0627@gmail.com>
2021-02-26 09:11:37 +0900

    server.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://lvh.me:5000',
+  uri: 'https://lvh.me:4000',
   cache: new InMemoryCache()
 });
 
